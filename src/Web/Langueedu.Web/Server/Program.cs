e125ad160097d7660f7d0b87ecb4ade31f663385
Ali <aldemirali93@gmail.com>
2022-01-12 06:28:29 +0300

    Initial web commit

---
+++ src/Web/Langueedu.Web/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+// Add services to the container.
+
+services.AddControllersWithViews();
+services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+//app.MapFallbackToFile("index.html");
+app.MapFallbackToPage("{*path:nonfile}", "/_Host");
+app.MapFallbackToPage("/", "/Landing");
+
+app.Run();
+
