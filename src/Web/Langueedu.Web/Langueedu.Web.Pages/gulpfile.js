bbf751af7c59fa5766adad5d69694c67b9293686
Ali <aldemirali93@gmail.com>
2022-01-12 12:39:57 +0300

    added gulp

---
+++ src/Web/Langueedu.Web/Langueedu.Web.Pages/gulpfile.js
@@ -0,0 +1,23 @@
+﻿const gulp = require("gulp")
+const sass = require('gulp-sass')(require('sass'));
+const watchSass = require("gulp-watch-sass")
+const concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+
+gulp.task("sass", () => gulp.src([
+    "./**/*.razor.scss",
+    "../Langueedu.Web.Components/**/*.razor.scss",
+    "../Langueedu.Web.Components/**/**/*.razor.scss"
+])
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest("./wwwroot/css")));
+
+gulp.task("watch", () => {
+    gulp.watch([
+        "./**/*.razor.scss",
+        "../Langueedu.Web.Components/**/*.razor.scss",
+        "../Langueedu.Web.Components/**/**/*.razor.scss"
+    ], gulp.series('sass'));
+});
--- src/Web/Langueedu.Web/Langueedu.Web.Pages/postcss.config.js
+++
@@ -1,6 +0,0 @@
-﻿module.exports = ({ env }) => ({
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
-});
