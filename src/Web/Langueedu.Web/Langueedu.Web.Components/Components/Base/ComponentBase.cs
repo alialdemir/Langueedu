dc6f90839feda209d8164f3b064c67020a581958
Ali <aldemirali93@gmail.com>
2022-01-13 12:04:45 +0300

    Binding components base lfecycle methods

---
+++ src/Web/Langueedu.Web/Langueedu.Web.Components/Components/Base/ComponentBase.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using Langueedu.Web.Components.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Langueedu.Web.Components;
+
+public class ComponentBase<T> : Microsoft.AspNetCore.Components.ComponentBase where T : ViewModelBase
+{
+    private IServiceProvider _serviceProvider;
+
+    protected internal T BindingContext { get; set; } = null!;
+
+    [Inject]
+    public IServiceProvider ServiceProvider
+    {
+        get => _serviceProvider;
+        set => _serviceProvider = value;
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        SetBindingContext();
+        BindingContext?.OnInitialized();
+    }
+
+    private void SetBindingContext()
+    {
+        BindingContext ??= _serviceProvider.GetRequiredService<T>();
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        return BindingContext?.OnInitializedAsync() ?? Task.CompletedTask;
+    }
+}
