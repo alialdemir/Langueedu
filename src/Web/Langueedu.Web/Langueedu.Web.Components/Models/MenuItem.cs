ae2a974377d4c7b6878f5809574877742842f1a9
Ali <aldemirali93@gmail.com>
2022-01-13 04:05:08 +0300

    main layout created

---
+++ src/Web/Langueedu.Web/Langueedu.Web.Components/Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Langueedu.Web.Components.Models;
+
+public class MenuItem
+{
+    public string Title { get; set; }
+    public string Icon { get; set; }
+    public string Href { get; set; }
+    public bool IsActive { get; set; }
+
+    public static List<MenuItem> MenuItems
+    {
+        get
+        {
+            return new List<MenuItem> {
+                        new MenuItem { Title = "Home", Href = "/", Icon = "pe-7s-home", IsActive = true },
+                        new MenuItem { Title = "Explore", Href = "/Explore", Icon = "pe-7s-compass", IsActive = false }
+                    };
+        }
+    }
+}
